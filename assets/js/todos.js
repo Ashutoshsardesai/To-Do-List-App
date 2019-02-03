025e348f67fe4a9e6a3fb0ad2d10f9fd28f5aeeb
Codebuster2599 <ashutoshsardesai2599@gmail.com>
2019-02-03 17:58:48 +0530

    mt commit

---
+++ assets/js/todos.js
@@ -0,0 +1,27 @@
+$("ul").on("click", "li", function()
+{
+	$(this).toggleClass("completed");
+	
+});
+$("ul").on("click", "span", function(event)
+{
+	$(this).parent().fadeOut(1000,function()
+		{
+			$(this).remove();
+		});
+	event.stopPropagation();
+
+})
+$("input[type='text'").keypress(function(event)
+{
+	if(event.which===13)
+	{
+		var todotext=$(this).val();
+		$(this).val("")
+		$("ul").append("<li><span><i class='fas fa-trash'></i></span>" + todotext + "</li>");
+	}
+})
+$(".fa-plus").click(function()
+{
+	$("input[type='text'").fadeToggle(500);
+})
